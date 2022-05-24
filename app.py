1bad8520273e1d4aafaaeaa9192aa27241b4585f
Baki <bakiguher@hotmail.com>
2022-05-24 10:51:11 +0200

    fixed app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from tensorflow.python.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image
 
 
-
-#model for mole class
+# model for mole class
 Modelmk_json = "./model/modelmk.json"
 Modelmk_weigths = "./model/modelmk.h5"
 
@@ -33,12 +32,13 @@ labels = {
 
 app = Flask(__name__)
 
+
 def take(n, iterable):
     "Return first n items of the iterable as a dict"
     return dict(islice(iterable, n))
 
 
-def get_MoleClassifierModel(modeljson,weights):
+def get_MoleClassifierModel(modeljson, weights):
     '''
     Function to load saved model and weights 
     '''
@@ -47,22 +47,20 @@ def get_MoleClassifierModel(modeljson,weights):
     model_json.close()
     model = model_from_json(loaded_model_json)
     model.load_weights(weights)
-    return model  
+    return model
 
 
-def model_predict(img:image, model,dima:int,dimb:int):
+def model_predict(img: image, model, dima: int, dimb: int):
     '''
     Get the image data and return predictions
     '''
     img = img.resize((dima, dimb))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x=x/255
-    #print(x)
+    x = x/255
     preds = model.predict(x)
-    
-    return preds
 
+    return preds
 
 
 @app.route('/', methods=['GET'])
@@ -76,50 +74,49 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     '''
-    predict function, makes the prediction and returns highest predicted in json format
+    predict function, loads the model calls the prediction and returns highest predicted 2 mole classes
     '''
     if request.method == 'POST':
         # Get the image from post request
         img = base64_to_pil(request.json)
-        
+
         # initialize models
         #model = get_MoleClassifierModel(Model_json,Model_weigths)
-        modelmk=get_MoleClassifierModel(Modelmk_json,Modelmk_weigths)
+        modelmk = get_MoleClassifierModel(Modelmk_json, Modelmk_weigths)
 
         # Make predictions
-        predsmk=model_predict(img,modelmk,32,32)
-        predsmk= np.around(predsmk*100,0)
+        predsmk = model_predict(img, modelmk, 32, 32)
+        predsmk = np.around(predsmk*100, 0)
         labelsof_type = list(labelsmk.values())
         predictions = predsmk.tolist()
 
-        #Round the results
-        dict1 = dict(zip(labelsof_type, np.around(predictions[0],2)))
-                  
-        
-        #sort the predictions descending
+        # Round the results
+        dict1 = dict(zip(labelsof_type, np.around(predictions[0], 2)))
+
+        # sort the predictions descending
         sorted_predictions = {}
-        sorted_keys = sorted(dict1, key=dict1.get,reverse=True) 
+        sorted_keys = sorted(dict1, key=dict1.get, reverse=True)
 
         for w in sorted_keys:
             sorted_predictions[w] = dict1[w]
-        
+
         for key, value in sorted_predictions.items():
             sorted_predictions[key] = '% ' + str(value)
-        
+
         # take top 2 predictions
         top2_predictions = take(2, sorted_predictions.items())
-        predictions= top2_predictions
+        predictions = top2_predictions
 
         # get percentages and names from dictionary and combine them
-        it_values = iter(predictions.values())    
-        firstv, secondv = next(it_values), next(it_values)    
+        it_values = iter(predictions.values())
+        firstv, secondv = next(it_values), next(it_values)
         it_keys = iter(predictions.keys())
         firstk, secondk = next(it_keys), next(it_keys)
-       
-        prediction1= firstv + " " + firstk
-        prediction2=secondv +" " + secondk
-        
-        return jsonify(result=prediction1,predresult=prediction2)
+
+        prediction1 = firstv + " " + firstk
+        prediction2 = secondv + " " + secondk
+
+        return jsonify(result=prediction1, predresult=prediction2)
     return None
 
 
